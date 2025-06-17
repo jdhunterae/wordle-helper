fa18804dfb8cccaca482bfbea73d3058e068ed8d
Andrew Pomerleau <arpomerleau@gmail.com>
2025-06-17 13:44:56 +0000

    Adding light-dark theming

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ document.getElementById('clear-button').addEventListener('click', clearBoard);
 document.getElementById('hint-button').addEventListener('click', applyHints);
 document.getElementById('toggle-known').addEventListener('click', applyHints);
 
+const themeToggle = document.getElementById("theme-toggle");
+themeToggle.addEventListener('change', () => {
+    document.documentElement.setAttribute('data-theme', themeToggle.checked ? 'dark' : 'light');
+});
+
 fetch("words.txt")
     .then(res => res.text())
     .then(text => {
